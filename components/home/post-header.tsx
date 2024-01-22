7c6d46d04727d5aad26bc96187fdaebf2cba3486
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2024-01-21 19:54:53 -0500

    Code formatted with Prettier

--- components/home/post-header.tsx
+++ components/home/post-header.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function PostHeader({
-    name,
-    verified,
-    createdAt,
+ name,
+ verified,
+ createdAt,
 }: {
-    name: string;
-    createdAt: string;
-    verified: boolean;
+ name: string;
+ createdAt: string;
+ verified: boolean;
 }) {
-    return (
-        <View>
-            <Text className='text-slate-100'>{name}</Text>
-            {verified && <MaterialIcons name="verified" size={14} color="#60a5fa" />}
-        </View>
-    );
+ return (
+  <View>
+   <Text className="text-slate-100">{name}</Text>
+   {verified && <MaterialIcons name="verified" size={14} color="#60a5fa" />}
+  </View>
+ );
 }
--- components/home/thread-item.tsx
+++ components/home/thread-item.tsx
@@ -2,16 +2,16 @@ import { Text, View } from 'react-native';
 import PostHeader from './post-header';
 
 export default function ThreadItem({ thread }: { thread: Thread }) {
-    return (
-        <View>
-            <Text className='text-slate-100'>{thread.author.username}</Text>
-            <View>
-                <PostHeader
-                    name={thread.author.name}
-                    createdAt={thread.createdAt}
-                    verified={thread.author.verified}
-                />
-            </View>
-        </View>
-    );
+ return (
+  <View>
+   <Text className="text-slate-100">{thread.author.username}</Text>
+   <View>
+    <PostHeader
+     name={thread.author.name}
+     createdAt={thread.createdAt}
+     verified={thread.author.verified}
+    />
+   </View>
+  </View>
+ );
 }
